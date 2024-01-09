132da5df06d76a8683244d1e3b866022820ea051
1110410030 <95633614+1110410030@users.noreply.github.com>
2024-01-09 15:42:28 +0800

    Add files via upload

---
+++ Finaltern/src/StarWar_1110410030/Main.java
@@ -0,0 +1,20 @@
+package StarWar_1110410030;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+class AirCraft {
+	}
+
+class XWing extends AirCraft {
+	
+}
+class TieFighter extends AirCraft{
+	
+}
+
+
+
+
+}
---
+++ Finaltern/src/module-info.java
@@ -0,0 +1,2 @@
+module Finaltern {
+}
